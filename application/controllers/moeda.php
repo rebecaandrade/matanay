54bcefb7d1921750097d22d25ae06c85c45f283d
Vitor Pontes <vitorpontes@cjr.org.br>
2015-05-08 01:12:55 -0300

    editar e deletar moeda

--- application/controllers/moeda.php
+++ application/controllers/moeda.php
@@ -18,20 +18,69 @@
 			$this->load->view('moeda/cadastro_moeda');
 		}
 		public function cadastrar_moeda(){
-			$nome = $this->input->post('nome');
-			$sigla = $this->input->post('sigla');
-			$cambio = $this->input->post('cambio');
-			
-			$this->moeda_model->cadastrar($nome,$sigla,$cambio);
+			// obtendo valores e removendo espaços em branco indesejaveis
+			$nome = trim($this->input->post('nome'));
+			$sigla = trim($this->input->post('sigla'));
+			$cambio = trim($this->input->post('cambio'));
+			if(strlen($nome) != 0 && strlen($sigla) != 0 && strlen($cambio) != 0){ //verificando se os campos foram preenchidos
+				$this->moeda_model->cadastrar($nome,$sigla,$cambio);
+			}
+			else{
+				//setar mensagem de campos vazios
+			}
 			redirect('moeda/cadastrar');
 		}
 		public function listar(){
-			// $this->session->set_flashdata('redirect_url', current_url());
-
-			// $linguagem_usuario = $this->session->userdata('linguagem');
-			// $this->lang->load('_matanay_'. $linguagem_usuario, $linguagem_usuario);
-			
 			$dados['moedas'] = $this->moeda_model->buscar_moedas();
 			$this->load->view('moeda/listar_moedas',$dados);
+		}
+		public function deletar(){
+			$id = $this->input->get("param");
+			if((string)(int)$id == $id){ //verifica se o ID é valido
+				$id = (int) $id;
+				$this->moeda_model->deletar_moeda($id);
+			}
+			redirect('moeda/listar');
+		}
+		public function editar(){
+			$id = $this->input->get('param');
+			if((string)(int)$id == $id){ // verifica se o ID é valido
+				//setar mensagem de sucesso
+				$dados['moeda'] = $this->moeda_model->buscar_moeda($id);
+				$this->load->view('moeda/editar_moeda',$dados);
+			}
+			else{
+				// seta mensagem de parametro invalido
+				redirect('moeda/listar');
+			}
+		}
+		public function editar_moeda(){
+			// obtendo valores e removendo espaços em branco indesejaveis
+			$id = trim($this->input->post('id'));
+			$nome = trim($this->input->post('nome'));
+			$sigla = trim($this->input->post('sigla'));
+			$cambio = trim($this->input->post('cambio'));
+			if(strlen($nome) != 0 && strlen($sigla) != 0 && strlen($cambio) != 0){ //verificando se campos foram preenchidos
+
+				if($this->moeda_model->editar_moeda($id,$nome,$sigla,$cambio)){
+				//seta mensagem de sucesso	
+				redirect('moeda/listar');
+				}
+				else{
+				//recarrega pagina de carregamento
+				$dados['moeda'] = $this->moeda_model->buscar_moeda($id);
+				
+				//setar mensagem de taxa de cambio invalida
+				$this->load->view('moeda/editar_moeda',$dados);
+			}
+			}
+			else{
+				//recarrega pagina de carregamento
+				$dados['moeda'] = $this->moeda_model->buscar_moeda($id);
+				
+				//setar mensagem de campos não preenchidos
+				$this->load->view('moeda/editar_moeda',$dados);
+			}
+
 		}
 	} 
