f44b344ecefc9daa156fee10a9154b9ffd73620e
evandrorobertomota <evandror7@hotmail.com>
2015-05-08 15:09:39 -0300

    confere tamanho do ISRC

--- application/models/faixas_videos_model.php
+++ application/models/faixas_videos_model.php
@@ -26,6 +26,11 @@ class Faixas_Videos_model extends CI_Model {
             $faixa['isrc'] = str_replace ("-", "", $faixa['isrc']);
         }
 
+        if(strlen($faixa['isrc']) != 12){
+            $this->session->set_userdata('mensagem', 'O código ISRC deve conter 12 caracteres.');
+            return FALSE;
+        }
+
         if(($faixa['percentual_artista'] + $faixa['percentual_autor'] + $faixa['percentual_produtor']) != 100){
             $this->session->set_userdata('mensagem', 'A soma das porcentagens não dá 100%');
             return FALSE;
