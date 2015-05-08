7c8de62a7fbd78e932fa5819c5662567fab2a3c0
Vitor Pontes <vitorpontes@cjr.org.br>
2015-05-08 15:03:25 -0300

    Underbar some apos hover

--- complemento/js/jquery.matanay.js
+++ complemento/js/jquery.matanay.js
@@ -3,6 +3,7 @@ $(function(){
     $(".opcao-menu").hover(function(){
         $("#trilho").width($(this).width());
         $("#trilho").css({"left": $(this).position().left });
+    },function(){
+        $("#trilho").width(0);
     });
 });
-	
