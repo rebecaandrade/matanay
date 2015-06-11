54eca1128546c327b5fb98083592afbfe1a6e86f
rebecaandrade <rebecaandrade@cjr.org.br>
2015-06-11 10:53:38 -0300

    script

---
+++ complemento/bd/script.sql
@@ -0,0 +1,15 @@
+/*tipo_favorecido*/
+insert into tipo_favorecido values("artista");
+insert into tipo_favorecido values("produtor");
+insert into tipo_favorecido values("autor");
+/*tipo_album*/
+insert into tipo_album values("coletanea");
+insert into tipo_album values("ao vivo");
+insert into tipo_album values("estudio");
+/*tipo_entidade*/
+insert into tipo_entidade values("artista");
+insert into tipo_entidade values("produtor");
+insert into tipo_entidade values("autor");
+/*tipo_modelo*/
+insert into tipo_modelo values("nacional");
+insert into tipo_modelo values("internacional");
