34d377a49c73ea6fa175ec777d1da588a9c35e39
rebecaandrade <rebecaandrade@cjr.org.br>
2015-06-11 11:07:25 -0300

    script

--- complemento/bd/script.sql
+++ complemento/bd/script.sql
@@ -1,15 +1,15 @@
 /*tipo_favorecido*/
-insert into tipo_favorecido values("artista");
-insert into tipo_favorecido values("produtor");
-insert into tipo_favorecido values("autor");
+insert into tipo_favorecido values(DEFAULT, "artista");
+insert into tipo_favorecido values(DEFAULT, "produtor");
+insert into tipo_favorecido values(DEFAULT, "autor");
 /*tipo_album*/
-insert into tipo_album values("coletanea");
-insert into tipo_album values("ao vivo");
-insert into tipo_album values("estudio");
+insert into tipo_album values(DEFAULT, "coletanea");
+insert into tipo_album values(DEFAULT, "ao vivo");
+insert into tipo_album values(DEFAULT, "estudio");
 /*tipo_entidade*/
-insert into tipo_entidade values("artista");
-insert into tipo_entidade values("produtor");
-insert into tipo_entidade values("autor");
+insert into tipo_entidade values(DEFAULT, "artista");
+insert into tipo_entidade values(DEFAULT, "produtor");
+insert into tipo_entidade values(DEFAULT, "autor");
 /*tipo_modelo*/
-insert into tipo_modelo values("nacional");
-insert into tipo_modelo values("internacional");
+insert into tipo_modelo values(DEFAULT, "nacional");
+insert into tipo_modelo values(DEFAULT, "internacional");
