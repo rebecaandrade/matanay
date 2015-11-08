8c42c779390868a083545ef5049afa1ab6f1418c
rodriggochaves <rodriggo.chaves@gmail.com>
2015-11-08 15:43:40 -0200

    script dos models

---
+++ application/models/modelScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mv acesso_model.php Acesso_model.php
+mv albuns_model.php Albuns_model.php
+mv cliente_model.php Cliente_model.php
+mv contrato_model.php Contrato_model.php
+mv entidade_model.php Entidade_model.php
+mv faixas_videos_model.php Faixas_videos_model.php
+mv favorecido_model.php Favorecido_model.php
+mv imposto_model.php Imposto_model.php
+mv modelo_relatorio_model.php Modelo_relatorio_model.php
+mv moeda_model.php Moeda_model.php
+mv notificacao_model.php Notificacao_model.php
+mv produtor_model.php Produtor_model.php
+mv relatorio_model.php Relatorio_model.php
